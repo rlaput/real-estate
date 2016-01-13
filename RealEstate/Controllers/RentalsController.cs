5ae70a600b786b5e19ba2a4288ced1a50af65094
Royce Beryl Laput <royce.laput@gmail.com>
2016-01-13 15:36:12 +0800

    Initial commit

---
+++ RealEstate/Controllers/RentalsController.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver.Builders;
+using RealEstate.App_Start;
+using RealEstate.Rentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class RentalsController : Controller
+    {
+        public readonly RealEstateContext Context = new RealEstateContext();
+
+        public ActionResult Index()
+        {
+            var rentals = Context.Rentals.FindAll();
+            return View(rentals);
+        }
+
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(PostRental postRental)
+        {
+            var rental = new Rental(postRental);
+            Context.Rentals.Insert(rental);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AdjustPrice(string id)
+        {
+            var rental = GetRental(id);
+            return View(rental);
+        }
+
+        private Rental GetRental(string id)
+        {
+            var rental = Context.Rentals.FindOneById(new ObjectId(id));
+            return rental;
+        }
+
+        [HttpPost]
+        public ActionResult AdjustPrice(string id, AdjustPrice adjustPrice)
+        {
+            var rental = GetRental(id);
+            rental.AdjustPrice(adjustPrice);
+            Context.Rentals.Save(rental);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            Context.Rentals.Remove(Query.EQ("_id", new ObjectId(id)));
+            return RedirectToAction("Index");
+        }
+    }
+}
